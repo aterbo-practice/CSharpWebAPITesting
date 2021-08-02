efc15ce2d56982b8a55330061a039d7a6f6206ec
andyterbovich <andy.terbovich@gmail.com>
2021-08-02 14:53:18 -0600

    added Get functions to controller

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -14,6 +14,19 @@ namespace MSLearnWebAPI.Controllers
         {
         }
         
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if(pizza == null)
+                return NotFound();
+            
+            return pizza;
+        }
     }
 
 }
