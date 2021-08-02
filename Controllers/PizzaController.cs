3fda3a4fe425acd726e514a1f1cc2f5829a06a9b
andyterbovich <andy.terbovich@gmail.com>
2021-08-02 14:27:29 -0600

    Added PizzaController.cs

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MSLearnWebAPI.Models;
+using MSLearnWebAPI.Services;
+
+namespace MSLearnWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+        }
+        
+    }
+
+}
